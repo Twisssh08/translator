b65db70acce9c7185c714f491eec8c3e98060266
Mateo Acosta <twishm08@gmail.com>
2025-04-24 21:18:52 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -35,7 +35,13 @@ st.subheader("Escucho lo que quieres traducir.")
 image = Image.open('catcatcat.png')
 
 st.image(image,width=300)
-with st.sidebar:
+st.markdown("""
+    <style>
+    {
+        background-color: #ffb343;  
+    }
+    </style>
+    """, unsafe_allow_html=True)
     st.subheader("Traductor.")
     st.write("Presiona el botón que dice ´escuchar´ "
                  "habla lo que quieres traducir, luego selecciona"   
